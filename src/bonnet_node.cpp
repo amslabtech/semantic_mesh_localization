834adf0a21b0d86d1193b1b38e8fb11f9b2219f3
Hibiki1020 <hibikijitaku@gmail.com>
2020-11-01 16:17:42 +0900

    tensorflow error

---
+++ src/bonnet_node.cpp
@@ -0,0 +1,18 @@
+#include <ros/ros.h>                                                                                 
+#include "semantic_mesh_localization/bonnet_handler.hpp"
+
+int main(int argc, char** argv) {
+  ros::init(argc, argv, "bonnet_node");
+  ros::NodeHandle nodeHandle("~");
+
+  bonnet::netHandler netH(nodeHandle);
+  bonnet::retCode status = netH.init();
+  if (status != bonnet::CNN_OK) {
+    ROS_ERROR("SOMETHING WENT WRONG INITIALIZING CNN. EXITING");
+    return 1;
+  }
+
+  ros::spin();
+  return 0;
+}
+
